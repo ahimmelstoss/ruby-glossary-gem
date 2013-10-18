e1f5f5cd4ef703e2cf7c73ea5b746e791dfa8ad7
ahimmelstoss <ahimmelstoss@gmail.com>
2013-10-18 15:34:09 -0400

    notes added

---
+++ notes.rb
@@ -0,0 +1,8 @@
+#scrape codeacademy ruby glossary (http://www.codecademy.com/glossary/ruby)
+#nokogiri the h3 glossary definition title to get the name of the method (eg, Array.flatten)
+#nokogiri the the p glossary definition definition to get the definition
+#also include (we will make up) examples for each
+#models: class is Glossary, and the instances being each topic (eg, arrays), and the attributes would be what you can do to the array (.flatten, .each, etc). ARRAYS = {:.each => "definition"}
+#puts all of this in a database
+#cli
+#wrapped in a gem
