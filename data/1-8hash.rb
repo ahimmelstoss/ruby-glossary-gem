391fa174c7c21e3b663dc402865ed86791d588a6
ahimmelstoss <ahimmelstoss@gmail.com>
2013-10-21 13:33:22 -0400

    added data directory

---
+++ data/1-8hash.rb
@@ -0,0 +1,41 @@
+glossary_terms = 
+	[
+		{
+			{:name=>"Creating Arrays"} => 
+				{
+					:definition => "the definition"
+				}, 
+				[
+					{
+						:syntax => "example"
+					},
+					{
+						:syntax => "example"
+					},
+					{
+						:syntax => "example"
+					}
+				]
+		},
+		{
+		"Array.each" => 
+			{
+				:definition => "the definition"
+			}, 
+			[
+				{
+					:syntax => "example"
+				},
+				{
+					:syntax => "example"
+				},
+				{
+					:syntax => "example"
+				}
+			]
+		}
+	]
+
+@term.name = glossary_terms.each do |name|
+	name[:name]=
+end
